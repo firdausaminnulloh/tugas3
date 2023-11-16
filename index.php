2b453cf6c046c149a8bb1f789bbbb5b97e8cb446
firdaus aminnulloh <firdaus_23510027@stimata.ac.id>
2023-11-16 20:37:15 +0700

    Menambahkan variabel nama

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+//  variabel dan tipe data
+// variabel = di gunakan untuk menyimpan data
+//aturan variabel 'tidak boleh di awali dengan angka,tapi boleh mengandung angka'
+
+$nama = "firdaus";
+?>
+
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Belajar php</title>
+</head>
+<body>
+    <h1>hallo,selamat datang<?php echo $nama; ?></h1>
+</body>
+</html>
